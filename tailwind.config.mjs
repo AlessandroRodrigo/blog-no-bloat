4e38a7361755f3c51fccb4d372aa8f001373adfd
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-11-02 11:06:22 -0300

    chore: reset styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#2337ff",
+        "accent-dark": "#000d8a",
+        black: "#0f1219",
+        gray: "#60739f",
+        "gray-light": "#e5e9f0",
+        "gray-dark": "#222939",
+      },
+    },
   },
   plugins: [],
 };
